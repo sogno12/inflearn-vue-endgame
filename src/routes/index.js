147b78a32a140945078cef934ce3084eab4794c2
Sujin <allure1212.c@gmail.com>
2020-08-26 11:08:21 +0900

    2_router commit

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
+
+Vue.use(VueRouter); //라우터 초기화
+
+export default new VueRouter({
+  mode: 'history', // 주소사이의 # 빼주기
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage.vue'),
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      path: '*', // 위의 path에 없는 경우 이쪽으로 모두 빠짐
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
