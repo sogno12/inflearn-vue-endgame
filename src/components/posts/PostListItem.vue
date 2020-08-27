c38d245f351af4f41f824f0a821c2b0210266d0a
Sujin <allure1212.c@gmail.com>
2020-08-27 11:48:39 +0900

    dateFormat Filter

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,15 +7,15 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
-      {{ postItem.createdAt }}
-      <i class="icon ion-md-create" @click="routEditItem"></i>
+      {{ postItem.createdAt | formatDate }}
+      <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
-    </div></li
-></template>
+    </div>
+  </li>
+</template>
 
 <script>
 import { deletePost } from '@/api/posts';
-
 export default {
   props: {
     postItem: {
@@ -25,13 +25,12 @@ export default {
   },
   methods: {
     async deleteItem() {
-      if (confirm('you want to delete it?')) {
+      if (confirm('You want to delete it?')) {
         await deletePost(this.postItem._id);
         this.$emit('refresh');
-        // console.log('삭제');
       }
     },
-    async routEditItem() {
+    routeEditPage() {
       const id = this.postItem._id;
       this.$router.push(`/post/${id}`);
     },
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index';
 import store from '@/store/index';
+import { formatDate } from '@/utils/filters.js';
 
+Vue.filter('formatDate', formatDate); // 전역 등록
 Vue.config.productionTip = false;
 
 new Vue({
