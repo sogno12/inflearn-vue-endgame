ae0d01d9a7f2223b47de99b404227faf7c154feb
Sujin <allure1212.c@gmail.com>
2020-08-27 13:18:57 +0900

    router-navigation-guard / logout

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div>
-      <router-link to="/" class="logo">
+      <router-link :to="logoLink" class="logo">
         TIL
         <span class="username" v-if="isUserLogin"
           >by {{ $store.state.username }}</span
@@ -23,16 +23,23 @@
 </template>
 
 <script>
+import { deleteCookie } from '@/utils/cookies';
 export default {
   computed: {
     isUserLogin() {
       return this.$store.getters.isLogin;
     },
+    logoLink() {
+      return this.$store.getters.isLogin ? '/main' : '/login';
+    },
   },
   methods: {
     logoutUser() {
-      this.$router.push('/login');
       this.$store.commit('clearUsername');
+      this.$store.commit('clearToken');
+      deleteCookie('til_auth');
+      deleteCookie('til_user');
+      this.$router.push('/login');
     },
   },
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 // import LoginPage from '@/views/LoginPage.vue';
 // import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter); //라우터 초기화
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history', // 주소사이의 # 빼주기
   routes: [
     {
@@ -23,14 +24,17 @@ export default new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '*', // 위의 path에 없는 경우 이쪽으로 모두 빠짐
@@ -38,3 +42,15 @@ export default new VueRouter({
     },
   ],
 });
+
+// 라우터 네이게이션 가드
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    console.log('인증이 필요합니다.');
+    next('/login');
+    return;
+  }
+  next();
+});
+
+export default router;
